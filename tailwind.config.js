51a2397f3b4f23c6e9f05f67eef1e7e5558e0b05
doubleun <nitsomboon77@gmail.com>
2023-04-21 22:42:19 +0700

    change background and add floating animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,6 +35,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      transitionDuration: {
+        5000: '5000ms',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
