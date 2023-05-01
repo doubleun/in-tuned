d509cadc48bd16323cb8541dec21c7120872f461
doubleun <nitsomboon77@gmail.com>
2023-05-02 02:44:13 +0700

    actually we use mock here

--- app/components/base/MarqueeText.tsx
+++ app/components/base/MarqueeText.tsx
@@ -8,7 +8,7 @@ const marqueeTextTwClass = {
 }
 
 async function MarqueeText() {
-  const channelData = (await getChannelDetails({ useMock: false })).items
+  const channelData = (await getChannelDetails({ useMock: true })).items
   const viewCounts = channelData[0].statistics.viewCount
 
   return (
--- app/components/compound/VideosSliderContainer.tsx
+++ app/components/compound/VideosSliderContainer.tsx
@@ -2,7 +2,7 @@ import { VideoSlider } from '@components/base'
 import { getFeaturedVideos } from '@service/youtube'
 
 async function VideosSliderContainer() {
-  const videos = (await getFeaturedVideos({ useMock: false })).items
+  const videos = (await getFeaturedVideos({ useMock: true })).items
   // console.log('vids: ', vids)
 
   return <VideoSlider videos={videos} />
